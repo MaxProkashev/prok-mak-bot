accd43f925abdd15f45cbe30762224e86d19d7e9
Prokashev Maxim <prokashev.m.p@gmail.com>
2020-09-20 22:43:50 +0300

    test

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/heroku/x v0.0.26
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
@@ -16,11 +17,29 @@ func main() {
 	} else {
 		log.Printf("[OK] Get PORT = %s", port)
 	}
-
+	// run gin router
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
-	router.Use(gin.Recovery())
-	router.Use(gin.Logger())
 
-	router.Run(":" + port)
+	// birth of a bot
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
+	if err != nil {
+		log.Fatalf("[X] Could not create bot. Reason: %s", err.Error())
+		return
+	}
+
+	// communication with the server
+	url := os.Getenv("COMMUNICATION_URL") + bot.Token
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(url))
+	if err != nil {
+		log.Fatalf("[X] Could not set webhook to bot settings. Reason: %s", err.Error())
+	}
+
+	router.POST("/" + bot.Token)
+
+	// run router
+	err = router.Run(":" + port)
+	if err != nil {
+		log.Fatalf("[X] Could not run router. Reason: %s", err.Error())
+	}
 }
